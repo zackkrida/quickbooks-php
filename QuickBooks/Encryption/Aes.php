29c97a9eb8ecb91aa6d51144b64588ff07618f43
zack krida <zackkrida@protonmail.com>
2019-04-04 14:49:00 -0400

    👌 IMPROVE: Skip mcrypt deprecation warning

--- QuickBooks/Encryption/Aes.php
+++ QuickBooks/Encryption/Aes.php
@@ -1,7 +1,7 @@
 <?php
 
 /** 
- * AES Encryption (depends on mcrypt for now)
+ * AES Encryption (depends on @mcrypt for now)
  * 
  * Copyright (c) 2010 Keith Palmer / ConsoliBYTE, LLC.
  * All rights reserved. This program and the accompanying materials
@@ -31,34 +31,34 @@ class QuickBooks_Encryption_AES extends QuickBooks_Encryption
 		
 		$plain = serialize(array( $plain, $salt ));
 		
-		$crypt = mcrypt_module_open('rijndael-256', '', 'ofb', '');
+		$crypt = @mcrypt_module_open('rijndael-256', '', 'ofb', '');
 
 		if (false !== stripos(PHP_OS, 'win') and 
 			version_compare(PHP_VERSION, '5.3.0')  == -1) 
 		{
-			$iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($crypt), MCRYPT_RAND);	
+			$iv = @mcrypt_create_iv(@mcrypt_enc_get_iv_size($crypt), MCRYPT_RAND);	
 		}
 		else
 		{
-			$iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($crypt), MCRYPT_DEV_URANDOM);
+			$iv = @mcrypt_create_iv(@mcrypt_enc_get_iv_size($crypt), MCRYPT_DEV_URANDOM);
 		}
 
-		$ks = mcrypt_enc_get_key_size($crypt);
+		$ks = @mcrypt_enc_get_key_size($crypt);
 		$key = substr(md5($key), 0, $ks);
 		
-		mcrypt_generic_init($crypt, $key, $iv);
-		$encrypted = base64_encode($iv . mcrypt_generic($crypt, $plain));
-		mcrypt_generic_deinit($crypt);
-		mcrypt_module_close($crypt);
+		@mcrypt_generic_init($crypt, $key, $iv);
+		$encrypted = base64_encode($iv . @mcrypt_generic($crypt, $plain));
+		@mcrypt_generic_deinit($crypt);
+		@mcrypt_module_close($crypt);
 		
 		return $encrypted;
 	}
 	
 	static function decrypt($key, $encrypted)
 	{
-		$crypt = mcrypt_module_open('rijndael-256', '', 'ofb', '');
-		$iv_size = mcrypt_enc_get_iv_size($crypt);
-		$ks = mcrypt_enc_get_key_size($crypt);
+		$crypt = @mcrypt_module_open('rijndael-256', '', 'ofb', '');
+		$iv_size = @mcrypt_enc_get_iv_size($crypt);
+		$ks = @mcrypt_enc_get_key_size($crypt);
 		$key = substr(md5($key), 0, $ks);
 		
 		//print('before base64 [' . $encrypted . ']' . '<br />');
@@ -70,10 +70,10 @@ class QuickBooks_Encryption_AES extends QuickBooks_Encryption
 		
 		//print('decrypting [' . $encrypted . ']' . '<br />');
 		
-		mcrypt_generic_init($crypt, $key, substr($encrypted, 0, $iv_size));
+		@mcrypt_generic_init($crypt, $key, substr($encrypted, 0, $iv_size));
 		$decrypted = trim(mdecrypt_generic($crypt, substr($encrypted, $iv_size)));
-		mcrypt_generic_deinit($crypt);
-		mcrypt_module_close($crypt);
+		@mcrypt_generic_deinit($crypt);
+		@mcrypt_module_close($crypt);
 		
 		//print('decrypted: [[**(' . $salt . ')');
 		//print_r($decrypted);
